3b8af2cbc4193f4ba281f81cd9d3c469db145c81
Manju9876 <manjunathb960650@gmail.com>
2026-05-07 15:00:14 +0530

    github actions practice

--- modules/rds/main.tf
+++ modules/rds/main.tf
@@ -1,15 +1,3 @@
-resource "aws_db_instance" "main" {
-  allocated_storage    = var.allocated_storage
-  db_name              = "${var.component_name}-dummy-${var.env}"
-  engine               = "postgres"
-  engine_version       = "16.3"
-  instance_class       = "db.t3.micro"
-  username             = "wmpuser"
-  password             = "WmpUser#1234"
-  parameter_group_name = aws_db_parameter_group.main.name
-  skip_final_snapshot  = true
-}
-
 resource "aws_db_parameter_group" "main" {
   name = "wmp-${var.env}"
   family = "postgres16"
@@ -22,4 +10,17 @@ resource "aws_db_subnet_group" "main" {
   tags = {
     Name = "wmp-${var.env}"
   }
+}
+
+resource "aws_db_instance" "main" {
+  allocated_storage    = var.allocated_storage
+  db_name              = "${var.component_name}-dummy-${var.env}"
+  engine               = "postgres"
+  engine_version       = "16.3"
+  instance_class       = "db.t3.micro"
+  username             = "wmpuser"
+  password             = "WmpUser#1234"
+  parameter_group_name = aws_db_parameter_group.main.name
+  skip_final_snapshot  = true
+  identifier = "wmp-${var.env}"
 }
