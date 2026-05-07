d268ad7fd3ed5ffa05657d97f2e03a9baec7d349
Manju9876 <manjunathb960650@gmail.com>
2026-05-07 21:57:26 +0530

    github actions  Prctice

--- modules/rds/main.tf
+++ modules/rds/main.tf
@@ -12,7 +12,33 @@ resource "aws_db_subnet_group" "main" {
   }
 }
 
+resource "aws_security_group" "main" {
+
+  name = "wmp-rds-sg-${var.env}"
+
+  ingress {
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "TCP"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "wmp-rds-sg-${var.env}"
+  }
+
+}
+
+
 resource "aws_db_instance" "main" {
+  identifier = "wmp-${var.env}"
   allocated_storage    = var.allocated_storage
   db_name              = "default_dummy"
   engine               = "postgres"
@@ -22,5 +48,6 @@ resource "aws_db_instance" "main" {
   password             = "WmpUser#1234"
   parameter_group_name = aws_db_parameter_group.main.name
   skip_final_snapshot  = true
-  identifier = "wmp-${var.env}"
+  vpc_security_group_ids = [aws_security_group.main.id]
+
 }
