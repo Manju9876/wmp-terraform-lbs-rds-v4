2806ec70653a7cf329646fae8c673fad9f014770
Manju9876 <manjunathb960650@gmail.com>
2026-05-08 12:36:50 +0530

    github actions practice

--- main.tf
+++ main.tf
@@ -18,11 +18,11 @@ module "apps" {
   alb_subnets = var.alb_subnets
 
 
-  for_each       = var.apps
-  component_name = each.key
-  instance_type  = each.value["instance_type"]
-  ports          = each.value["ports"]
-  alb            = each.value["alb"]
-  asg            = each.value["asg"]
-  aws_rds_endpoint_address = module.database.aws_rds_endpoint_address
+  for_each                 = var.apps
+  component_name           = each.key
+  instance_type            = each.value["instance_type"]
+  ports                    = each.value["ports"]
+  alb                      = each.value["alb"]
+  asg                      = each.value["asg"]
+  aws_rds_endpoint_address = module.database["postgresql"].aws_rds_endpoint_address
 }
