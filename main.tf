39ba6c3fc753a4d5e1b2b7a7349178b21b24b2ab
Manju9876 <manjunathb960650@gmail.com>
2026-05-08 10:52:18 +0530

    github actions practice

--- main.tf
+++ main.tf
@@ -7,21 +7,21 @@ module "database" {
   env            = var.env
   db_subnets = var.db_subnets
 }
-#
-# module "apps" {
-#   depends_on = [module.database]
-#   source = "./modules/component-with-alb"
-#
-#   dns_domain  = var.dns_domain
-#   env         = var.env
-#   vpc_id      = var.vpc_id
-#   alb_subnets = var.alb_subnets
-#
-#
-#   for_each       = var.apps
-#   component_name = each.key
-#   instance_type  = each.value["instance_type"]
-#   ports          = each.value["ports"]
-#   alb            = each.value["alb"]
-#   asg            = each.value["asg"]
-# }
+
+module "apps" {
+  depends_on = [module.database]
+  source = "./modules/component-with-alb"
+
+  dns_domain  = var.dns_domain
+  env         = var.env
+  vpc_id      = var.vpc_id
+  alb_subnets = var.alb_subnets
+
+
+  for_each       = var.apps
+  component_name = each.key
+  instance_type  = each.value["instance_type"]
+  ports          = each.value["ports"]
+  alb            = each.value["alb"]
+  asg            = each.value["asg"]
+}
--- modules/component-with-alb/user_data.sh
+++ modules/component-with-alb/user_data.sh
@@ -5,7 +5,7 @@ sudo dnf install -y python3-pip
 sudo pip3 install ansible hvac
 #sudo python3.11 -m pip install ansible hvac
 ansible-pull -i localhost, \
-  -U https://github.com/Manju9876/wmp-ansible \
+  -U https://github.com/Manju9876/wmp-ansible-rds-v2 \
   wmp.yaml \
   -e env=${ENV} \
   -e component_name=${COMPONENT}
